8399656399800d12c2032facfac13deecce9182c
Peter Perlepes <p.perlepes@gmail.com>
2021-06-28 10:51:39 +0300

    fix: remove useRecipes warning

--- client/hooks/useRecipes.ts
+++ client/hooks/useRecipes.ts
@@ -1,4 +1,6 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
 import { useUser } from "@clerk/clerk-react";
 import { useEffect, useState } from "react";
 import type { Recipe } from "../types";
